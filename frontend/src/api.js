18b899dfbc9874f1d1f60be45f06b0b724369560
sarthakagarwal8737-ctrl <sarthakagarwal8737@gmail.com>
2026-09-07 20:57:03 +0530

    Final URL adding

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 const API_BASE =
   import.meta.env.VITE_API_BASE_URL ||
-  "http://localhost:5000/api";
+  "https://sih26044-wooc.onrender.com/api";
 
 
 // ============================================================
